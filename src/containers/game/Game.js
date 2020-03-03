7b87606808e859341b87d1a3398f56f806419095
Roberta <roberta_stefana1998@yahoo.com>
2020-03-03 10:42:38 +0200

    Add initial game view for admin

--- src/containers/game/Game.js
+++ src/containers/game/Game.js
@@ -1,17 +1,92 @@
 import React, { Component } from 'react';
+import {logoList} from '../../static/logo'
 import{ 
-    Grid, Typography
+    Grid, Typography, Button
 } from '@material-ui/core'
 import {PlayersTable} from '../../components';
 
+
+const team1 = {name: 'CS Universitatea NBS Cluj'}
+const team2 = {name: 'CSS Gloria Arad'}
+
 class Game extends Component {
-    state = {  }
+    state = { 
+        imageTeam1 :null,
+        imageTeam2: null,
+    }
+
+    componentDidMount(){
+       
+        logoList.map(l=>{
+            if(l.team === team1.name){
+                this.setState({
+                    imageTeam1: l.img
+                })
+            }else if(l.team === team2.name){
+                this.setState({
+                    imageTeam2: l.img
+                })
+            }
+        })
+    }
+
     render() { 
+        const {classes} = this.props;
+        const {imageTeam1, imageTeam2} = this.state;
+
         return ( 
             <Grid container>
-                <Grid item>
-                    <Typography>llalala</Typography>
+                <Grid item xs={12} className={classes.teams}>
+                    <div className={classes.teamNameLeft}>
+                        <img src={imageTeam1} alt="logo" className={classes.logo}/>
+                        <Typography variant="h5">{team1.name}</Typography>
+                    </div>
+                    <div className={classes.score}>
+                        <Typography variant ="h4">25 : 65</Typography>
+                    </div>
+                    <div className={classes.teamNameRight}>
+                        <img src={imageTeam2} alt="logo" className={classes.logo}></img>
+                        <Typography variant="h5">{team2.name}</Typography>
+                    </div>
+                </Grid>
+                <Grid item xs={5} className={`${classes.tableContainerLeft} ${classes.flexColumn}`}>
                     <PlayersTable></PlayersTable>
+                </Grid>
+                <Grid item xs={2} >
+                    <Typography>Time</Typography>
+                    <Typography>Quater</Typography>
+                </Grid>
+                <Grid item xs={5} className={`${classes.tableContainerRight} ${classes.flexColumn}`}>
+                    <PlayersTable></PlayersTable>
+                </Grid>
+                <Grid item xs={12} className={classes.buttonContainer}>
+                    <div className={classes.buttonSection}>
+                        <Button className={classes.button}>+1</Button>
+                        <Button className={classes.button}>+2</Button>
+                        <Button className={classes.button}>+3</Button>
+                        <Button className={classes.button}>Miss 1</Button>
+                        <Button className={classes.button}>Miss 2</Button>
+                        <Button className={classes.button}>Miss 3</Button>
+
+                        <Button className={classes.button}>Off Reb</Button>
+                        <Button className={classes.button}>Def Reb</Button>
+                        <Button className={classes.button}>Bs</Button>
+                        <Button className={classes.button}>As</Button>
+                        <Button className={classes.button}>To</Button>
+                        <Button className={classes.button}>St</Button>
+                        <Button className={classes.button}>PF</Button>
+                        <Button className={classes.button}>FD</Button>
+                    </div>
+                    <div className={classes.wrapper}>
+                    <div className={classes.timeButtons}>
+                        <Button className={classes.buttonSquare}>Start Time</Button>
+                        <Button className={classes.buttonSquare}>Stop Time</Button>
+                        <Button className={classes.buttonSquare}>Timeout</Button>
+                        <Button className={classes.buttonSquare}>Start Game</Button>
+                    </div>
+                    </div>
+
+                    
                 </Grid>
             </Grid>     
         );
--- src/containers/game/styles.js
+++ src/containers/game/styles.js
@@ -1,7 +1,82 @@
-import { createStyles } from '@material-ui/core';
+import { createStyles, withTheme } from '@material-ui/core';
 
 const styles = theme => createStyles({
-  // add styles here
+  	teams:{
+		height: '65px',
+		display: 'flex',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		borderBottom: '5px groove',
+		marginBottom: '30px',
+	},
+	logo:{
+        height:50,
+		width:60,
+		paddingRight: '10px',
+	},
+	teamNameLeft:{
+		width: '320px',
+		display: 'flex',
+		justifyContent: 'flex-start',
+	},
+	teamNameRight:{
+		width: '300px',
+		display: 'flex',
+		justifyContent: 'flex-end'
+	},	
+	buttonContainer:{
+		paddingTop:'30px',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+
+	},
+	buttonSection:{
+		display: 'flex',
+		flexFlow: 'row wrap',
+		position: 'relative',
+		width:'700px',
+		justifyContent:'space-around'
+	},
+	wrapper:{
+		width: '400px',
+		height: '200px',
+	},
+	timeButtons:{
+		display:'flex',
+		flexDirection:'row',
+		flexFlow: 'row wrap',
+		position: 'relative',
+		justifyContent:'space-around',
+		width:'300px',
+		margin:'auto',
+		height:'100%'
+	},
+	button:{
+		width:'80px',
+		height: '80px',
+		borderRadius:'100%',
+		margin: '8px',
+		backgroundColor: theme.palette.primary.dark,
+		color: theme.palette.common.white,
+	},
+	buttonSquare:{
+		width:120,
+		height:60,
+		backgroundColor: theme.palette.primary.main,
+		color: theme.palette.primary.dark,
+	},
+	flexColumn:{
+		display:'flex',
+		flexDirection:'column',
+	},
+	tableContainerLeft:{
+		alignItems:'flex-start',
+	},
+	tableContainerRight:{
+		alignItems:'flex-end',
+	},
+
 });
 
 export default styles;
